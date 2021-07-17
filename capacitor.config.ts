f0f25bd2f55655177762ad0a97725e29413808fd
Phil Custer <phil.custer@procore.com>
2021-07-17 18:42:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wif-impulse-tracker',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
